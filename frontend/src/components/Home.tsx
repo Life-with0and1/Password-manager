e4c57fc7d493f8ec903d5d6738a7f66fd80c72b4
Ayush Kumar <ayuuayuu0se1@gmail.com>
2024-12-07 22:30:53 +0530

    Home.tsx

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 import axios from "axios";
 
 const Home = () => {
-  const url = "http://localhost:4000/api/credential";
+  const url = "https://pass-store-backend.onrender.com/api/credential";
   const [credentils, setCredentials] = useState([]);
 
   const fetchCredentials = async () => {
